e5dbe3deb7f06859b214d10e06407312d3fae756
munovrizall <muhammadnovrizal87@gmail.com>
2023-01-23 21:45:06 +0700

    Signed-off-by: Mohammad Novrizal <muhammadnovrizal87@gmail.com>

---
+++ Toolbar-NavigationGraph/app/src/main/java/com/artonov/catchat/InboxFragment.kt
@@ -0,0 +1,20 @@
+package com.artonov.catchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artonov.catchat.databinding.FragmentInboxBinding
+
+class InboxFragment : Fragment() {
+    lateinit var binding: FragmentInboxBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inbox, container, false)
+    }
+}
---
+++ Toolbar-NavigationGraph/app/src/main/java/com/artonov/catchat/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.artonov.catchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.artonov.catchat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val config = AppBarConfiguration(navController.graph)
+        binding.toolBar.setupWithNavController(navController, config)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+}
