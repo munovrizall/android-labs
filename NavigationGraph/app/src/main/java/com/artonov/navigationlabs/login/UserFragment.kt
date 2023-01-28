ac1a97057b3480bab4917eda93856e0e5ea1b86a
munovrizall <muhammadnovrizal87@gmail.com>
2023-01-28 14:53:54 +0700

    Login completed

--- NavigationGraph/app/src/main/java/com/artonov/navigationlabs/login/UserFragment.kt
+++ NavigationGraph/app/src/main/java/com/artonov/navigationlabs/login/UserFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.artonov.navigationlabs.R
+import com.artonov.navigationlabs.calculate.OutputFragmentArgs
+import com.artonov.navigationlabs.databinding.FragmentLoginBinding
+import com.artonov.navigationlabs.databinding.FragmentUserBinding
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +23,26 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class UserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentUserBinding? = null
+    private val binding get() = _binding!!
+    private val args : UserFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val username = args.username
+        val password = args.password
+
+        binding.tvUsername.text = username + password
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
 }
