6984fa0f91f2648c2e39c7ca91081edd88409347
munovrizall <muhammadnovrizal87@gmail.com>
2023-02-11 22:20:39 +0700

    Styling completed

--- HFAD/Chapter 11 - ViewModel/CalculatorLabs/app/src/main/java/com/artonov/calculatorlabs/MainActivity.kt
+++ HFAD/Chapter 11 - ViewModel/CalculatorLabs/app/src/main/java/com/artonov/calculatorlabs/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.artonov.calculatorlabs
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.artonov.calculatorlabs.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var binding: ActivityMainBinding
     lateinit var viewModel: MainViewModel
 
@@ -21,12 +27,40 @@ class MainActivity : AppCompatActivity() {
 
         binding.tvHasil.text = viewModel.result.toString()
 
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.operations_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerOperations.adapter = adapter
+        }
+
+        binding.spinnerOperations.onItemSelectedListener = this
+
         binding.btnCalculate.setOnClickListener() {
             var angka1 = binding.etAngka1.text.toString().toInt()
             var angka2 = binding.etAngka2.text.toString().toInt()
             viewModel.result = angka1 * angka2
 
             binding.tvHasil.text = viewModel.result.toString()
+            showToast(message = "Halo")
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        when (view?.id) {
+            0 -> showToast(message = "Plus selected")
+            1 -> showToast(message = "Minus selected")
+
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        showToast(message = "Nothing selected")
+    }
+    private fun showToast(context: Context = applicationContext, message: String, duration: Int = Toast.LENGTH_LONG) {
+        Toast.makeText(context, message, duration).show()
     }
 }
